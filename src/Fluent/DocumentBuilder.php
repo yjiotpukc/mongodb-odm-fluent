71660497b7633d969aa7c71df9a0795ef92e146b
Vadim Zubko <yjiotpukc@gmail.com>
2021-10-14 20:48:03 +0300

    Adds methods to DocumentBuilder

--- src/Fluent/DocumentBuilder.php
+++ src/Fluent/DocumentBuilder.php
@@ -5,11 +5,90 @@ declare(strict_types=1);
 namespace yjiotpukc\MongoODMFluent\Fluent;
 
 use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
+use yjiotpukc\MongoODMFluent\Types\Index;
 
 class DocumentBuilder extends BaseBuilder implements FluentBuilder
 {
+    protected $db;
+    protected $collection;
+    protected $repositoryClass;
+    protected $readOnly;
+    protected $writeConcern;
+    protected $indexes;
+
     public function build(ClassMetadata $metadata): void
     {
+        if ($this->db) {
+            $metadata->setDatabase($this->db);
+        }
+        if ($this->collection) {
+            $metadata->setCollection($this->collection);
+        }
+        if ($this->repositoryClass) {
+            $metadata->setCustomRepositoryClass($this->repositoryClass);
+        }
+        if ($this->readOnly) {
+            $metadata->isReadOnly = $this->readOnly;
+        }
+        if ($this->writeConcern) {
+            $metadata->setWriteConcern($this->writeConcern);
+        }
+        if ($this->indexes) {
+            foreach ($this->indexes as $index) {
+                $metadata->addIndex($index->keys, $index->options());
+            }
+        }
+
         parent::build($metadata);
+    }
+
+    public function db(string $name): DocumentBuilder
+    {
+        $this->db = $name;
+
+        return $this;
+    }
+
+    public function collection(string $name): DocumentBuilder
+    {
+        $this->collection = $name;
+
+        return $this;
+    }
+
+    public function repository(string $className): DocumentBuilder
+    {
+        $this->repositoryClass = $className;
+
+        return $this;
+    }
+
+    public function readOnly(): DocumentBuilder
+    {
+        $this->readOnly = true;
+
+        return $this;
+    }
+
+    /**
+     * @param string|int|null $writeConcern
+     */
+    public function writeConcern($writeConcern): DocumentBuilder
+    {
+        $this->writeConcern = $writeConcern;
+
+        return $this;
+    }
+
+    /**
+     * @param string|string[] $keys
+     * @return Index
+     */
+    public function index($keys = []): Index
+    {
+        $index = new Index($keys);
+        $this->indexes[] = $index;
+
+        return $index;
     }
 }
