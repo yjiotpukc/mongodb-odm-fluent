7c3f8a6cd17b80d0f4134867f6480addc0112550
Vadim Zubko <yjiotpukc@gmail.com>
2021-10-26 22:32:15 +0300

    Fixed tests for finders

--- tests/MappingFinder/DirectoryMappingFinderTest.php
+++ tests/MappingFinder/DirectoryMappingFinderTest.php
@@ -5,52 +5,44 @@ declare(strict_types=1);
 namespace yjiotpukc\MongoODMFluent\Tests\MappingFinder;
 
 use PHPUnit\Framework\TestCase;
-use yjiotpukc\MongoODMFluent\MappingException;
 use yjiotpukc\MongoODMFluent\MappingFinder\DirectoryMappingFinder;
+use yjiotpukc\MongoODMFluent\MappingSet\MappingSet;
 use yjiotpukc\MongoODMFluent\Tests\Stubs\EntityStub;
-use yjiotpukc\MongoODMFluent\Tests\Stubs\EntityWithoutMappingStub;
 use yjiotpukc\MongoODMFluent\Tests\Stubs\Mappings\AnotherEntityStub;
+use yjiotpukc\MongoODMFluent\Tests\Stubs\Mappings\AnotherMappingStub;
 use yjiotpukc\MongoODMFluent\Tests\Stubs\MappingStub;
 
 class DirectoryMappingFinderTest extends TestCase
 {
     public const NAMESPACE = 'yjiotpukc\MongoODMFluent\Tests\Stubs';
 
-    public function testMappingExists()
+    public function testFindsMappings()
     {
-        $finder = new DirectoryMappingFinder([$this->getDirectoryPath()], [static::NAMESPACE]);
-        self::assertTrue($finder->exists(EntityStub::class));
+        $mappingSet = $this->createMappingSet();
+        self::assertEquals(MappingStub::class, $mappingSet->find(EntityStub::class));
     }
 
-    public function testMappingDoesNotExist()
+    public function testFindsMappingsInSubdirectories()
     {
-        $finder = new DirectoryMappingFinder([$this->getDirectoryPath()], [static::NAMESPACE]);
-        self::assertFalse($finder->exists(EntityWithoutMappingStub::class));
+        $mappingSet = $this->createMappingSet();
+        self::assertEquals(AnotherMappingStub::class, $mappingSet->find(AnotherEntityStub::class));
     }
 
-    public function testReturnsMapping()
+    public function testFindsOnlyMappings()
     {
-        $finder = new DirectoryMappingFinder([$this->getDirectoryPath()], [static::NAMESPACE]);
-        self::assertEquals(MappingStub::class, $finder->find(EntityStub::class));
-    }
-
-    public function testFailsIfNoMappingFound()
-    {
-        $entityClassName = EntityWithoutMappingStub::class;
-        $this->expectException(MappingException::class);
-        $this->expectExceptionMessage("Mapping for entity [$entityClassName] not found");
-        $finder = new DirectoryMappingFinder([$this->getDirectoryPath()], [static::NAMESPACE]);
-        $finder->find($entityClassName);
-    }
-
-    public function testReturnsAllMappings()
-    {
-        $finder = new DirectoryMappingFinder([$this->getDirectoryPath()], [static::NAMESPACE]);
-        self::assertEquals([EntityStub::class, AnotherEntityStub::class], $finder->getAll());
+        $mappingSet = $this->createMappingSet();
+        self::assertEquals([EntityStub::class, AnotherEntityStub::class], $mappingSet->getAll());
     }
 
     protected function getDirectoryPath(): string
     {
         return realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Stubs');
+    }
+
+    public function createMappingSet(): MappingSet
+    {
+        $finder = new DirectoryMappingFinder([$this->getDirectoryPath()], [self::NAMESPACE]);
+
+        return $finder->makeMappingSet();
     }
 }
