dca7e5a0e6312606bf007ec1ae86a625bd20d055
Vadim Zubko <yjiotpukc@gmail.com>
2021-10-26 21:53:22 +0300

    Adds MappingSet

---
+++ src/MappingSet/SimpleMappingSet.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace yjiotpukc\MongoODMFluent\MappingSet;
+
+use yjiotpukc\MongoODMFluent\MappingException;
+
+class SimpleMappingSet implements MappingSet
+{
+    protected $mappings;
+
+    public function __construct()
+    {
+        $this->mappings = [];
+    }
+
+    public function add(string $entityClassName, string $mappingClassName)
+    {
+        $this->mappings[$entityClassName] = $mappingClassName;
+    }
+
+    public function exists(string $entityClassName): bool
+    {
+        return isset($this->mappings[$entityClassName]);
+    }
+
+    public function find(string $entityClassName): string
+    {
+        if (!$this->exists($entityClassName)) {
+            throw new MappingException("Mapping for entity [$entityClassName] not found");
+        }
+
+        return $this->mappings[$entityClassName];
+    }
+
+    public function getAll(): array
+    {
+        return array_keys($this->mappings);
+    }
+}
