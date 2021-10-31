d366e3f8a5c28bf6cb4a1495e59fabb10a049521
Vadim Zubko <yjiotpukc@gmail.com>
2021-10-31 21:07:35 +0300

    Adds Db buildable

--- src/Builder/Traits/CanHaveDb.php
+++ src/Builder/Traits/CanHaveDb.php
@@ -4,26 +4,16 @@ declare(strict_types=1);
 
 namespace yjiotpukc\MongoODMFluent\Builder\Traits;
 
-use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
+use yjiotpukc\MongoODMFluent\Buildable\Db;
 
 trait CanHaveDb
 {
-    /**
-     * @var string
-     */
-    protected $db;
-
     public function db(string $name): self
     {
-        $this->db = $name;
+        $this->addBuildable(new Db($name));
 
         return $this;
     }
 
-    protected function buildDb(ClassMetadata $metadata)
-    {
-        if ($this->db) {
-            $metadata->setDatabase($this->db);
-        }
-    }
+    abstract protected function addBuildable($buildable);
 }
