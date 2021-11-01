646a102f1e47de43ab8ddae5edda1f82844d8189
Vadim Zubko <yjiotpukc@gmail.com>
2021-11-01 18:31:54 +0300

    Adds tests for fields

--- tests/Unit/Builder/MappedSuperclassBuilderTest.php
+++ tests/Unit/Builder/MappedSuperclassBuilderTest.php
@@ -7,6 +7,7 @@ namespace yjiotpukc\MongoODMFluent\Tests\Unit\Builder;
 use yjiotpukc\MongoODMFluent\Builder\MappedSuperclassBuilder;
 use yjiotpukc\MongoODMFluent\Tests\Unit\Builder\Traits\TestCollection;
 use yjiotpukc\MongoODMFluent\Tests\Unit\Builder\Traits\TestDb;
+use yjiotpukc\MongoODMFluent\Tests\Unit\Builder\Traits\TestField;
 use yjiotpukc\MongoODMFluent\Tests\Unit\Builder\Traits\TestId;
 
 class MappedSuperclassBuilderTest extends BuilderBaseTestCase
@@ -14,6 +15,7 @@ class MappedSuperclassBuilderTest extends BuilderBaseTestCase
     use TestDb;
     use TestCollection;
     use TestId;
+    use TestField;
 
     public function givenEmptyBuilder(): MappedSuperclassBuilder
     {
