22d526764dc1c769691438791f45f576fe1e0223
Vadim Zubko <yjiotpukc@gmail.com>
2021-11-01 17:30:06 +0300

    Changes tests for builders

--- tests/Unit/Builder/BuilderBaseTestCase.php
+++ tests/Unit/Builder/BuilderBaseTestCase.php
@@ -4,79 +4,34 @@ declare(strict_types=1);
 
 namespace yjiotpukc\MongoODMFluent\Tests\Unit\Builder;
 
+use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
 use PHPUnit\Framework\TestCase;
-use yjiotpukc\MongoODMFluent\Buildable\EmbedMany;
-use yjiotpukc\MongoODMFluent\Buildable\EmbedOne;
-use yjiotpukc\MongoODMFluent\Buildable\Field;
-use yjiotpukc\MongoODMFluent\Buildable\Id;
-use yjiotpukc\MongoODMFluent\Buildable\ReferenceMany;
-use yjiotpukc\MongoODMFluent\Buildable\ReferenceOne;
-use yjiotpukc\MongoODMFluent\Tests\Stubs\BaseBuilderStub;
+use yjiotpukc\MongoODMFluent\Tests\Stubs\EntityStub;
 
 abstract class BuilderBaseTestCase extends TestCase
 {
-    /**
-     * @dataProvider methodProvider
-     */
-    public function testBuilderAddsField(BaseBuilderStub $builder, string $method, array $arguments)
-    {
-        $field = $builder->$method(...$arguments);
-
-        self::assertTrue(in_array($field, $builder->getFields(), true));
-    }
-
-    /**
-     * @dataProvider methodProvider
-     */
-    public function testBuilderAddsOnlyOneField(BaseBuilderStub $builder, string $method, array $arguments)
-    {
-        $previousCount = count($builder->getFields());
-
-        $builder->$method(...$arguments);
-        $currentCount = count($builder->getFields());
-
-        self::assertEquals(1, $currentCount - $previousCount);
-    }
-
-    /**
-     * @dataProvider methodProvider
-     */
-    public function testBuilderDoesNotModifyConstructorArguments(BaseBuilderStub $builder, string $method, array $arguments, string $className)
-    {
-        $expected = new $className(...$arguments);
-
-        $field = $builder->$method(...$arguments);
-
-        self::assertEquals($expected, $field);
-    }
-
-    public function methodProvider(): array
-    {
-        return [
-            [$this->givenEmptyBuilder(), 'id', [], Id::class],
-            [$this->givenEmptyBuilder(), 'field', ['type', 'fieldName'], Field::class],
-            [$this->givenEmptyBuilder(), 'embedOne', ['field', 'target'], EmbedOne::class],
-            [$this->givenEmptyBuilder(), 'embedMany', ['field', 'target'], EmbedMany::class],
-            [$this->givenEmptyBuilder(), 'referenceOne', ['field', 'target'], ReferenceOne::class],
-            [$this->givenEmptyBuilder(), 'referenceMany', ['field', 'target'], ReferenceMany::class],
-
-            [$this->givenBuilderWithId(), 'field', ['type', 'fieldName'], Field::class],
-            [$this->givenBuilderWithId(), 'embedOne', ['field', 'target'], EmbedOne::class],
-            [$this->givenBuilderWithId(), 'embedMany', ['field', 'target'], EmbedMany::class],
-            [$this->givenBuilderWithId(), 'referenceOne', ['field', 'target'], ReferenceOne::class],
-            [$this->givenBuilderWithId(), 'referenceMany', ['field', 'target'], ReferenceMany::class],
-
-            [$this->givenBuilderWithSomeFields(), 'field', ['type', 'fieldName'], Field::class],
-            [$this->givenBuilderWithSomeFields(), 'embedOne', ['field', 'target'], EmbedOne::class],
-            [$this->givenBuilderWithSomeFields(), 'embedMany', ['field', 'target'], EmbedMany::class],
-            [$this->givenBuilderWithSomeFields(), 'referenceOne', ['field', 'target'], ReferenceOne::class],
-            [$this->givenBuilderWithSomeFields(), 'referenceMany', ['field', 'target'], ReferenceMany::class],
-        ];
-    }
-
     abstract public function givenEmptyBuilder();
 
-    abstract public function givenBuilderWithId();
+    public function givenBuilderWithId()
+    {
+        $builder = $this->givenEmptyBuilder();
+        $builder->id();
 
-    abstract public function givenBuilderWithSomeFields();
+        return $builder;
+    }
+
+    public function givenBuilderWithSomeFields()
+    {
+        $builder = $this->givenEmptyBuilder();
+        $builder->field('string', 'firstName');
+        $builder->field('string', 'lastName');
+        $builder->field('int', 'age');
+
+        return $builder;
+    }
+
+    public function givenClassMetadata(): ClassMetadata
+    {
+        return new ClassMetadata(EntityStub::class);
+    }
 }
