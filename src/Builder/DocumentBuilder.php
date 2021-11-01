d84f0689f477824adbd78b950c5ccaf452efbd3c
Vadim Zubko <yjiotpukc@gmail.com>
2021-11-01 17:02:32 +0300

    Moves fields to a trait

--- src/Builder/DocumentBuilder.php
+++ src/Builder/DocumentBuilder.php
@@ -10,6 +10,7 @@ use yjiotpukc\MongoODMFluent\Builder\Traits\CanHaveCollection;
 use yjiotpukc\MongoODMFluent\Builder\Traits\CanHaveDb;
 use yjiotpukc\MongoODMFluent\Builder\Traits\CanHaveDiscriminator;
 use yjiotpukc\MongoODMFluent\Builder\Traits\CanHaveEmbeds;
+use yjiotpukc\MongoODMFluent\Builder\Traits\CanHaveFields;
 use yjiotpukc\MongoODMFluent\Builder\Traits\CanHaveIndex;
 use yjiotpukc\MongoODMFluent\Builder\Traits\CanHaveInheritance;
 use yjiotpukc\MongoODMFluent\Builder\Traits\CanHaveReferences;
@@ -19,6 +20,7 @@ class DocumentBuilder extends BaseBuilder implements Buildable
 {
     use CanHaveDb;
     use CanHaveCollection;
+    use CanHaveFields;
     use CanHaveReferences;
     use CanHaveEmbeds;
     use CanHaveIndex;
