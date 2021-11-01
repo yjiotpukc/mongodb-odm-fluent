e88c793eaf693311d1d47c55ef2f5d5842b4f9db
Vadim Zubko <yjiotpukc@gmail.com>
2021-11-01 16:59:46 +0300

    Moves embeds to a trait

--- src/Builder/BaseBuilder.php
+++ src/Builder/BaseBuilder.php
@@ -6,18 +6,10 @@ namespace yjiotpukc\MongoODMFluent\Builder;
 
 use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
 use yjiotpukc\MongoODMFluent\Buildable\Buildable;
-use yjiotpukc\MongoODMFluent\Buildable\EmbedMany as EmbedManyImplementation;
-use yjiotpukc\MongoODMFluent\Buildable\EmbedOne as EmbedOneImplementation;
 use yjiotpukc\MongoODMFluent\Buildable\Field as FieldImplementation;
 use yjiotpukc\MongoODMFluent\Buildable\Id as IdImplementation;
-use yjiotpukc\MongoODMFluent\Buildable\ReferenceMany as ReferenceManyImplementation;
-use yjiotpukc\MongoODMFluent\Buildable\ReferenceOne as ReferenceOneImplementation;
-use yjiotpukc\MongoODMFluent\Type\EmbedMany;
-use yjiotpukc\MongoODMFluent\Type\EmbedOne;
 use yjiotpukc\MongoODMFluent\Type\Field;
 use yjiotpukc\MongoODMFluent\Type\Id;
-use yjiotpukc\MongoODMFluent\Type\ReferenceMany;
-use yjiotpukc\MongoODMFluent\Type\ReferenceOne;
 
 abstract class BaseBuilder implements Buildable
 {
@@ -41,16 +33,6 @@ abstract class BaseBuilder implements Buildable
     public function field(string $type, string $fieldName): Field
     {
         return $this->addBuildable(new FieldImplementation($type, $fieldName));
-    }
-
-    public function embedOne(string $fieldName, string $target = ''): EmbedOne
-    {
-        return $this->addBuildable(new EmbedOneImplementation($fieldName, $target));
-    }
-
-    public function embedMany(string $fieldName, string $target = ''): EmbedMany
-    {
-        return $this->addBuildable(new EmbedManyImplementation($fieldName, $target));
     }
 
     protected function addBuildable($buildable)
---
+++ src/Builder/Traits/CanHaveEmbeds.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace yjiotpukc\MongoODMFluent\Builder\Traits;
+
+use yjiotpukc\MongoODMFluent\Buildable\EmbedMany as EmbedManyImplementation;
+use yjiotpukc\MongoODMFluent\Buildable\EmbedOne as EmbedOneImplementation;
+use yjiotpukc\MongoODMFluent\Type\EmbedMany;
+use yjiotpukc\MongoODMFluent\Type\EmbedOne;
+
+trait CanHaveEmbeds
+{
+    public function embedOne(string $fieldName, string $target = ''): EmbedOne
+    {
+        return $this->addBuildable(new EmbedOneImplementation($fieldName, $target));
+    }
+
+    public function embedMany(string $fieldName, string $target = ''): EmbedMany
+    {
+        return $this->addBuildable(new EmbedManyImplementation($fieldName, $target));
+    }
+
+    abstract protected function addBuildable($buildable);
+}
