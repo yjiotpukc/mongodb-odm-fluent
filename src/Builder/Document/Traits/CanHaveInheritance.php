6e6cbc22ab56bcc6396df8b95155e839643c3d1e
Vadim Zubko <yjiotpukc@gmail.com>
2021-11-03 19:32:29 +0300

    Uses static constructors instead of constants in CanHaveInheritance trait

--- src/Builder/Document/Traits/CanHaveInheritance.php
+++ src/Builder/Document/Traits/CanHaveInheritance.php
@@ -4,18 +4,17 @@ declare(strict_types=1);
 
 namespace yjiotpukc\MongoODMFluent\Builder\Document\Traits;
 
-use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
 use yjiotpukc\MongoODMFluent\Builder\Database\Inheritance;
 
 trait CanHaveInheritance
 {
     public function singleCollection(): self
     {
-        return $this->addBuilderAndReturnSelf(new Inheritance(ClassMetadata::INHERITANCE_TYPE_SINGLE_COLLECTION));
+        return $this->addBuilderAndReturnSelf(Inheritance::singleCollection());
     }
 
     public function collectionPerClass(): self
     {
-        return $this->addBuilderAndReturnSelf(new Inheritance(ClassMetadata::INHERITANCE_TYPE_COLLECTION_PER_CLASS));
+        return $this->addBuilderAndReturnSelf(Inheritance::collectionPerClass());
     }
 }
