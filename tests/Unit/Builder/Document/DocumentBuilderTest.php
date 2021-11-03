e8230a0fdf8389604b23078fffbafe95753e6b6d
Vadim Zubko <yjiotpukc@gmail.com>
2021-11-03 19:54:43 +0300

    Moves document builder tests to appropriate namespace

--- tests/Unit/Builder/Document/DocumentBuilderTest.php
+++ tests/Unit/Builder/Document/DocumentBuilderTest.php
@@ -2,11 +2,12 @@
 
 declare(strict_types=1);
 
-namespace yjiotpukc\MongoODMFluent\Tests\Unit\Builder;
+namespace yjiotpukc\MongoODMFluent\Tests\Unit\Builder\Document;
 
 use yjiotpukc\MongoODMFluent\Builder\Document\DocumentBuilder;
 use yjiotpukc\MongoODMFluent\Tests\Stubs\Mappings\AnotherEntityStub;
 use yjiotpukc\MongoODMFluent\Tests\Stubs\RepositoryStub;
+use yjiotpukc\MongoODMFluent\Tests\Unit\Builder\BuilderTestCase;
 use yjiotpukc\MongoODMFluent\Tests\Unit\Builder\Field\EmbedManyTest;
 use yjiotpukc\MongoODMFluent\Tests\Unit\Builder\Field\EmbedOneTest;
 use yjiotpukc\MongoODMFluent\Tests\Unit\Builder\Field\FieldTest;
