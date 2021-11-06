48e53874b756ff6ac9bc2c90d6e5b4c0d38bef7b
Vadim Zubko <yjiotpukc@gmail.com>
2021-11-06 14:52:47 +0300

    Replaces EmbeddedDocumentBuilder with EmbeddedDocument interface

--- src/Mapping/EmbeddedDocumentMapping.php
+++ src/Mapping/EmbeddedDocumentMapping.php
@@ -5,16 +5,18 @@ declare(strict_types=1);
 namespace yjiotpukc\MongoODMFluent\Mapping;
 
 use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
-use yjiotpukc\MongoODMFluent\Builder\Document\EmbeddedDocumentBuilder;
+use yjiotpukc\MongoODMFluent\Builder\Document\DocumentBuilder;
+use yjiotpukc\MongoODMFluent\Builder\EmbeddedDocument;
 
 abstract class EmbeddedDocumentMapping implements Mapping
 {
     public function load(ClassMetadata $metadata): void
     {
-        $builder = new EmbeddedDocumentBuilder();
+        $builder = new DocumentBuilder();
+        $builder->embeddedDocument();
         $this->map($builder);
         $builder->build($metadata);
     }
 
-    abstract public function map(EmbeddedDocumentBuilder $builder): void;
+    abstract public function map(EmbeddedDocument $builder): void;
 }
