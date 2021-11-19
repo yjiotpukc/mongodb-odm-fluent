c236763e1202872113c44db353b4b17fb6dca713
Vadim Zubko <yjiotpukc@gmail.com>
2021-11-19 03:03:32 +0300

    Update document builders interfaces

--- src/Builder/Document.php
+++ src/Builder/Document.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace yjiotpukc\MongoODMFluent\Builder;
 
+use yjiotpukc\MongoODMFluent\Type\ChangeTrackingPolicy;
 use yjiotpukc\MongoODMFluent\Type\Collection;
 use yjiotpukc\MongoODMFluent\Type\Discriminator;
 use yjiotpukc\MongoODMFluent\Type\EmbedMany;
@@ -11,31 +12,46 @@ use yjiotpukc\MongoODMFluent\Type\EmbedOne;
 use yjiotpukc\MongoODMFluent\Type\Field;
 use yjiotpukc\MongoODMFluent\Type\Id\Id;
 use yjiotpukc\MongoODMFluent\Type\Index;
+use yjiotpukc\MongoODMFluent\Type\IntegerField;
+use yjiotpukc\MongoODMFluent\Type\ReadPreferenceMode;
 use yjiotpukc\MongoODMFluent\Type\ReferenceMany;
 use yjiotpukc\MongoODMFluent\Type\ReferenceOne;
+use yjiotpukc\MongoODMFluent\Type\Shard;
 
 interface Document
 {
     public function db(string $name): Document;
 
-    public function collection(string $name): Collection;
+    public function repository(string $className): Document;
+
+    public function readOnly(): Document;
 
     public function singleCollection(): Document;
 
     public function collectionPerClass(): Document;
-
-    public function repository(string $className): Document;
-
-    public function readOnly(): Document;
 
     /**
      * @param int|string|null $writeConcern
      */
     public function writeConcern($writeConcern): Document;
 
-    public function id(): Id;
+    public function readPreference(): ReadPreferenceMode;
 
-    public function field(string $type, string $fieldName): Field;
+    public function collection(string $name): Collection;
+
+    /**
+     * @param string|string[] $keys
+     * @return Index
+     */
+    public function index($keys = []): Index;
+
+    public function discriminator(string $field): Discriminator;
+
+    public function shard(): Shard;
+
+    public function changeTrackingPolicy(): ChangeTrackingPolicy;
+
+    public function id(): Id;
 
     public function referenceOne(string $fieldName, string $target = ''): ReferenceOne;
 
@@ -45,11 +61,43 @@ interface Document
 
     public function embedMany(string $fieldName, string $target = ''): EmbedMany;
 
-    public function discriminator(string $field): Discriminator;
+    public function field(string $type, string $fieldName): Field;
 
-    /**
-     * @param string|string[] $keys
-     * @return Index
-     */
-    public function index($keys = []): Index;
+    public function string(string $fieldName): Field;
+
+    public function int(string $fieldName): IntegerField;
+
+    public function float(string $fieldName): Field;
+
+    public function bool(string $fieldName): Field;
+
+    public function timestamp(string $fieldName): Field;
+
+    public function date(string $fieldName): Field;
+
+    public function dateImmutable(string $fieldName): Field;
+
+    public function decimal(string $fieldName): Field;
+
+    public function array(string $fieldName): Field;
+
+    public function hash(string $fieldName): Field;
+
+    public function key(string $fieldName): Field;
+
+    public function objectId(string $fieldName): Field;
+
+    public function raw(string $fieldName): Field;
+
+    public function bin(string $fieldName): Field;
+
+    public function binBytearray(string $fieldName): Field;
+
+    public function binCustom(string $fieldName): Field;
+
+    public function binFunc(string $fieldName): Field;
+
+    public function binMd5(string $fieldName): Field;
+
+    public function binUuid(string $fieldName): Field;
 }
