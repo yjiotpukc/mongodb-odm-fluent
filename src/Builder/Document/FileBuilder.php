9929ef4df0935c66001d2bd6eace1092e091ab5a
Vadim Zubko <yjiotpukc@gmail.com>
2021-11-21 03:11:40 +0300

    Adds file builder

---
+++ src/Builder/Document/FileBuilder.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+namespace yjiotpukc\MongoODMFluent\Builder\Document;
+
+use yjiotpukc\MongoODMFluent\Builder\BaseBuilder;
+use yjiotpukc\MongoODMFluent\Builder\Database\BucketBuilder;
+use yjiotpukc\MongoODMFluent\Builder\Database\ChunkSizeBuilder;
+use yjiotpukc\MongoODMFluent\Builder\Database\DbBuilder;
+use yjiotpukc\MongoODMFluent\Builder\Database\IndexBuilder;
+use yjiotpukc\MongoODMFluent\Builder\Database\ReadOnlyBuilder;
+use yjiotpukc\MongoODMFluent\Builder\Database\ReadPreferenceBuilder;
+use yjiotpukc\MongoODMFluent\Builder\Database\RepositoryClassBuilder;
+use yjiotpukc\MongoODMFluent\Builder\Database\ShardBuilder;
+use yjiotpukc\MongoODMFluent\Builder\Database\WriteConcernBuilder;
+use yjiotpukc\MongoODMFluent\Builder\Field\FieldBuilder;
+use yjiotpukc\MongoODMFluent\Builder\Field\FileMetadataBuilder;
+use yjiotpukc\MongoODMFluent\Builder\Field\IdBuilder;
+use yjiotpukc\MongoODMFluent\Builder\File;
+use yjiotpukc\MongoODMFluent\Type\FileMetadata;
+use yjiotpukc\MongoODMFluent\Type\Index;
+use yjiotpukc\MongoODMFluent\Type\ReadPreferenceMode;
+use yjiotpukc\MongoODMFluent\Type\Shard;
+
+class FileBuilder extends BaseBuilder implements File
+{
+    public function db(string $name): FileBuilder
+    {
+        return $this->addBuilderAndReturnSelf(new DbBuilder($name));
+    }
+
+    public function bucket(string $name): File
+    {
+        return $this->addBuilderAndReturnSelf(new BucketBuilder($name));
+    }
+
+    public function repository(string $className): FileBuilder
+    {
+        return $this->addBuilderAndReturnSelf(new RepositoryClassBuilder($className));
+    }
+
+    public function readOnly(): FileBuilder
+    {
+        return $this->addBuilderAndReturnSelf(new ReadOnlyBuilder());
+    }
+
+    public function chunkSize(int $bytes): File
+    {
+        return $this->addBuilderAndReturnSelf(new ChunkSizeBuilder($bytes));
+    }
+
+    /**
+     * @param int|string|null $writeConcern
+     */
+    public function writeConcern($writeConcern): FileBuilder
+    {
+        return $this->addBuilderAndReturnSelf(new WriteConcernBuilder($writeConcern));
+    }
+
+    public function readPreference(): ReadPreferenceMode
+    {
+        return $this->addBuilder(new ReadPreferenceBuilder());
+    }
+
+    /**
+     * @param string|string[] $keys
+     */
+    public function index($keys = []): Index
+    {
+        return $this->addBuilder(new IndexBuilder($keys));
+    }
+
+    public function shard(): Shard
+    {
+        return $this->addBuilder(new ShardBuilder());
+    }
+
+    public function id(): IdBuilder
+    {
+        return $this->addBuilder(new IdBuilder());
+    }
+
+    public function filenameFieldName(string $fieldName): File
+    {
+        $filename = new FieldBuilder('string', $fieldName);
+        $filename->nameInDb('filename')->notSaved();
+
+        return $this->addBuilderAndReturnSelf($filename);
+    }
+
+    public function uploadDateFieldName(string $fieldName): File
+    {
+        $uploadDate = new FieldBuilder('date', $fieldName);
+        $uploadDate->nameInDb('uploadDate')->notSaved();
+
+        return $this->addBuilderAndReturnSelf($uploadDate);
+    }
+
+    public function lengthFieldName(string $fieldName): File
+    {
+        $length = new FieldBuilder('int', $fieldName);
+        $length->nameInDb('length')->notSaved();
+
+        return $this->addBuilderAndReturnSelf($length);
+    }
+
+    public function chunkSizeFieldName(string $fieldName): File
+    {
+        $length = new FieldBuilder('int', $fieldName);
+        $length->nameInDb('chunkSize')->notSaved();
+
+        return $this->addBuilderAndReturnSelf($length);
+    }
+
+    public function metadata(): FileMetadata
+    {
+        return $this->addBuilder(new FileMetadataBuilder());
+    }
+}
