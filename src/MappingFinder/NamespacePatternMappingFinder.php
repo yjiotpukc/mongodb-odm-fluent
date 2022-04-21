4007cae3c8e04025f0734e6db02c3f2d06be644b
yjiotpukc <yjiotpukc@gmail.com>
2022-04-21 19:43:37 +0300

    Moves directory scanning to util class

--- src/MappingFinder/NamespacePatternMappingFinder.php
+++ src/MappingFinder/NamespacePatternMappingFinder.php
@@ -7,66 +7,41 @@ namespace yjiotpukc\MongoODMFluent\MappingFinder;
 use yjiotpukc\MongoODMFluent\Mapping\Mapping;
 use yjiotpukc\MongoODMFluent\MappingSet\MappingSet;
 use yjiotpukc\MongoODMFluent\MappingSet\SimpleMappingSet;
+use yjiotpukc\MongoODMFluent\Util\DirectoryScanner;
 
 class NamespacePatternMappingFinder implements MappingFinder
 {
     protected string $entityClassReplacementPattern;
     protected string $mappingClassPattern;
-    protected string $mappingDirectory;
-    protected bool $isDirectoryScanned = false;
+    protected DirectoryScanner $directoryScanner;
 
     public function __construct(string $mappingClassPattern, string $entityClassReplacementPattern, string $mappingDirectory)
     {
         $this->mappingClassPattern = $mappingClassPattern;
         $this->entityClassReplacementPattern = $entityClassReplacementPattern;
-        $this->mappingDirectory = $mappingDirectory;
+        $this->directoryScanner = new DirectoryScanner($mappingDirectory);
     }
 
     public function makeMappingSet(): MappingSet
     {
-        $this->scanDirectory();
+        $scannedClasses = $this->directoryScanner->scanDirectory();
+
+        return $this->createMappingSet($scannedClasses);
+    }
+
+    protected function createMappingSet(array $classes): MappingSet
+    {
         $mappingSet = new SimpleMappingSet();
-        $this->addMappingsToSet($mappingSet);
+        foreach ($classes as $className) {
+            if (
+                preg_match($this->mappingClassPattern, $className)
+                && in_array(Mapping::class, class_implements($className), true)
+            ) {
+                $entityClassName = preg_replace($this->mappingClassPattern, $this->entityClassReplacementPattern, $className);
+                $mappingSet->add($entityClassName, $className);
+            }
+        }
 
         return $mappingSet;
-    }
-
-    protected function scanDirectory(): void
-    {
-        if (!$this->isDirectoryScanned) {
-            $this->scanDir($this->mappingDirectory);
-            $this->isDirectoryScanned = true;
-        }
-    }
-
-    protected function scanDir(string $dirPath): void
-    {
-        $inner = scandir($dirPath);
-        foreach ($inner as $item) {
-            if ($item === '.' || $item === '..') {
-                continue;
-            }
-
-            $absolutePath = $dirPath . DIRECTORY_SEPARATOR . $item;
-            if (is_dir($absolutePath)) {
-                $this->scanDir($absolutePath);
-            } elseif (str_ends_with($absolutePath, '.php')) {
-                require_once $absolutePath;
-            }
-        }
-    }
-
-    protected function addMappingsToSet(SimpleMappingSet $mappingSet): void
-    {
-        $declaredClasses = get_declared_classes();
-        foreach ($declaredClasses as $declaredClass) {
-            if (preg_match($this->mappingClassPattern, $declaredClass)) {
-                $instance = new $declaredClass();
-                if ($instance instanceof Mapping) {
-                    $entityClassName = preg_replace($this->mappingClassPattern, $this->entityClassReplacementPattern, $declaredClass);
-                    $mappingSet->add($entityClassName, $declaredClass);
-                }
-            }
-        }
     }
 }
