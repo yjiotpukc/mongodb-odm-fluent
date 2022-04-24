7210eb603fe915abae5bf18b6b46e4e2ec6f5a97
yjiotpukc <yjiotpukc@gmail.com>
2022-04-24 20:38:53 +0300

    Replaces "extends QueryResultDocumentMapping" with "implements QueryResultDocument"

--- tests/Integration/Resources/Mappings/PhonesCountMapping.php
+++ tests/Integration/Resources/Mappings/PhonesCountMapping.php
@@ -5,9 +5,8 @@ declare(strict_types=1);
 namespace yjiotpukc\MongoODMFluent\Tests\Integration\Resources\Mappings;
 
 use yjiotpukc\MongoODMFluent\Builder\QueryResultDocument;
-use yjiotpukc\MongoODMFluent\Mapping\QueryResultDocumentMapping;
 
-class PhonesCountMapping extends QueryResultDocumentMapping
+class PhonesCountMapping implements \yjiotpukc\MongoODMFluent\Document\QueryResultDocument
 {
     public function map(QueryResultDocument $builder): void
     {
