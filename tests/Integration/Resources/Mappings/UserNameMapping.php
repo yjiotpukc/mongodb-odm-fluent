75e6afa62c138fa3a4c195215818c341da37fc1a
yjiotpukc <yjiotpukc@gmail.com>
2022-04-24 20:39:09 +0300

    Replaces "extends ViewMapping" with "implements View"

--- tests/Integration/Resources/Mappings/UserNameMapping.php
+++ tests/Integration/Resources/Mappings/UserNameMapping.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 namespace yjiotpukc\MongoODMFluent\Tests\Integration\Resources\Mappings;
 
 use yjiotpukc\MongoODMFluent\Builder\View;
-use yjiotpukc\MongoODMFluent\Mapping\ViewMapping;
 use yjiotpukc\MongoODMFluent\Tests\Integration\Resources\Entities\User;
 use yjiotpukc\MongoODMFluent\Tests\Integration\Resources\Repositories\UserNameRepository;
 
-class UserNameMapping extends ViewMapping
+class UserNameMapping implements \yjiotpukc\MongoODMFluent\Document\View
 {
     public function map(View $builder): void
     {
