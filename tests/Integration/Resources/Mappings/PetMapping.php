e76588fdb11c2f8c388597b3109805cd6a8b7987
yjiotpukc <yjiotpukc@gmail.com>
2022-04-24 20:38:06 +0300

    Replaces "extends MappedSuperclassMapping" with "implements MappedSuperclass"

--- tests/Integration/Resources/Mappings/PetMapping.php
+++ tests/Integration/Resources/Mappings/PetMapping.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace yjiotpukc\MongoODMFluent\Tests\Integration\Resources\Mappings;
 
 use yjiotpukc\MongoODMFluent\Builder\Document;
-use yjiotpukc\MongoODMFluent\Mapping\MappedSuperclassMapping;
+use yjiotpukc\MongoODMFluent\Document\MappedSuperclass;
 use yjiotpukc\MongoODMFluent\Tests\Integration\Resources\Entities\Bird;
 use yjiotpukc\MongoODMFluent\Tests\Integration\Resources\Entities\Dog;
 
-class PetMapping extends MappedSuperclassMapping
+class PetMapping implements MappedSuperclass
 {
     public function map(Document $builder): void
     {
