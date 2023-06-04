df3267428a738006ddc48a5bf252a8d130f63119
yjiotpukc <yjiotpukc@gmail.com>
2023-06-04 18:41:50 +0300

    Adds test for loading embedded document

--- src/Loader/ClassMetadataLoader.php
+++ src/Loader/ClassMetadataLoader.php
@@ -6,6 +6,7 @@ namespace yjiotpukc\MongoODMFluent\Loader;
 
 use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
 use yjiotpukc\MongoODMFluent\Builder\Document\DocumentBuilder;
+use yjiotpukc\MongoODMFluent\Document\EmbeddedDocument;
 use yjiotpukc\MongoODMFluent\MappingSet\MappingSet;
 
 class ClassMetadataLoader
@@ -13,6 +14,12 @@ class ClassMetadataLoader
     public function load(string $mapping, ClassMetadata $metadata): void
     {
         $builder = new DocumentBuilder();
+
+        $implements = class_implements($mapping);
+        if (in_array(EmbeddedDocument::class, $implements)) {
+            $builder->embeddedDocument();
+        }
+
         $mapping::map($builder);
         $builder->build($metadata);
     }
--- tests/Unit/Loader/ClassMetadataLoaderTest.php
+++ tests/Unit/Loader/ClassMetadataLoaderTest.php
@@ -8,6 +8,7 @@ use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
 use PHPUnit\Framework\TestCase;
 use yjiotpukc\MongoODMFluent\Loader\ClassMetadataLoader;
 use yjiotpukc\MongoODMFluent\Tests\Stubs\EntityStub;
+use yjiotpukc\MongoODMFluent\Tests\Stubs\Mappings\EmbeddedEntityStubMapping;
 use yjiotpukc\MongoODMFluent\Tests\Stubs\Mappings\EntityStubMapping;
 
 class ClassMetadataLoaderTest extends TestCase
@@ -17,7 +18,30 @@ class ClassMetadataLoaderTest extends TestCase
         $entity = EntityStub::class;
         $mapping = EntityStubMapping::class;
         $loader = new ClassMetadataLoader();
-        $loader->load($mapping, new ClassMetadata($entity));
-        self::assertTrue(EntityStubMapping::wasLoaded());
+        $metadata = new ClassMetadata($entity);
+        $loader->load($mapping, $metadata);
+
+        $this->assertTrue(EntityStubMapping::wasLoaded());
+        $this->assertFalse($metadata->isEmbeddedDocument);
+        $this->assertFalse($metadata->isMappedSuperclass);
+        $this->assertFalse($metadata->isFile);
+        $this->assertFalse($metadata->isView());
+        $this->assertFalse($metadata->isQueryResultDocument);
+    }
+
+    public function testLoadsEmbeddedDocumentMetadata(): void
+    {
+        $entity = EntityStub::class;
+        $mapping = EmbeddedEntityStubMapping::class;
+        $loader = new ClassMetadataLoader();
+        $metadata = new ClassMetadata($entity);
+        $loader->load($mapping, $metadata);
+
+        $this->assertTrue(EmbeddedEntityStubMapping::wasLoaded());
+        $this->assertTrue($metadata->isEmbeddedDocument);
+        $this->assertFalse($metadata->isMappedSuperclass);
+        $this->assertFalse($metadata->isFile);
+        $this->assertFalse($metadata->isView());
+        $this->assertFalse($metadata->isQueryResultDocument);
     }
 }
