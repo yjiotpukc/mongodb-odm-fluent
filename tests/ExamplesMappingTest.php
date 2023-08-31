062e20ba7ef17a64e3865a9bd16b36e01c136e84
yjiotpukc <yjiotpukc@gmail.com>
2023-08-31 23:16:09 +0300

    Removes unit tests

--- tests/ExamplesMappingTest.php
+++ tests/ExamplesMappingTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace yjiotpukc\MongoODMFluent\Tests\Integration;
+namespace yjiotpukc\MongoODMFluent\Tests;
 
 use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
@@ -10,17 +10,12 @@ use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
 use Doctrine\Persistence\Mapping\Driver\StaticPHPDriver;
 use PHPUnit\Framework\TestCase;
 use yjiotpukc\MongoODMFluent\AnnotationCompatibleMetadataConverter;
-use yjiotpukc\MongoODMFluent\Examples\Mapping\Entity\Inheritance\Animal;
-use yjiotpukc\MongoODMFluent\Examples\Mapping\Entity\Inheritance\Crocodile;
-use yjiotpukc\MongoODMFluent\Examples\Mapping\Entity\Inheritance\Hatiko;
-use yjiotpukc\MongoODMFluent\Examples\Mapping\Entity\Inheritance\Mammal;
-use yjiotpukc\MongoODMFluent\Examples\Mapping\Entity\Inheritance\Reptile;
 
 class ExamplesMappingTest extends TestCase
 {
     public function testMapping(): void
     {
-        $entitiesDir = __DIR__ . '/../../examples/Mapping/Entity';
+        $entitiesDir = __DIR__ . '/../examples/Mapping/Entity';
 
         $annotationDriver = new AnnotationDriver(new AnnotationReader(), $entitiesDir);
         $phpDriver = new StaticPHPDriver($entitiesDir);
